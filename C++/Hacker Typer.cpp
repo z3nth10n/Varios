50f3269e5e349a3a262f213c29f38fafeffb05e5
ivancea <ivancea@mundo-r.com>
2014-09-05 21:16:48 +0200

    Added line number

--- C++/Hacker Typer.cpp
+++ C++/Hacker Typer.cpp
@@ -8,6 +8,21 @@ using namespace std;
 
 #define CHARS_NUM 2
 
+void coutIt(char* str, size_t n){
+    static size_t line=2;
+    bool contains=false;
+    int i=0, t=0;
+    while(i<n){
+        if(str[i]=='\n'){
+            cout.write(str+t,i-t);
+            cout << endl << line++ << "  ";
+            t=i+1;
+        }
+        ++i;
+    }
+    cout.write(str+t,i-t);
+}
+
 int main(int args, char** argc){
     if(args==1){
         cout << "Por lo menos 1 archivo de parametro." << endl;
@@ -15,13 +30,14 @@ int main(int args, char** argc){
         return 1;
     }
     char *chars = new char[CHARS_NUM];
+    cout << "1  ";
     for(int i=1; i<args; i++){
         ifstream file(argc[i]);
         if(!file) continue;
         while(file){
             getch();
             file.read(chars,CHARS_NUM);
-            cout.write(chars,file.gcount());
+            coutIt(chars,file.gcount());
         }
         cout << endl;
     }
