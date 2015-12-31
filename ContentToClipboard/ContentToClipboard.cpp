f74a586f9445872ba86a245944b7be3e8a156932
ivancea <ivancea@mundo-r.com>
2015-12-31 15:45:06 +0100

    Added uninstall option
    Now, you can uninstall it

--- ContentToClipboard/ContentToClipboard.cpp
+++ ContentToClipboard/ContentToClipboard.cpp
@@ -1,3 +1,4 @@
+#define  _WIN32_WINNT  0x0600
 #include <windows.h>
 #include <iostream>
 #include <fstream>
@@ -14,6 +15,15 @@ int regAdd(HKEY type, std::string ruta, const char* valor, std::string data){
 	}
 	RegCloseKey(key);
 	return 0;
+}
+
+void regDelKey(HKEY type, std::string ruta, std::string subKey){
+    HKEY key;
+    long lng = RegOpenKey(type,ruta.c_str(),&key);
+    if(lng == ERROR_SUCCESS){
+        RegDeleteTree(key, subKey.c_str());
+        RegCloseKey(key);
+    }
 }
 
 void showError(const char* error){
@@ -71,17 +81,28 @@ int copyFile(const char* fileName){
 int main(int argc, char** argv){
 	const char* contextMenuName = "Copiar contenido"; // For multi-language purposes
 	int errorLevel = 0;
-	if(argc == 1 && MessageBox(NULL, "Do you want to install this program? (Requires explicit admin rights)", "ContentToClipboard::Install", MB_YESNO) == IDYES){
+	if(argc == 1){
 		char buffer[MAX_PATH]={0};
-		GetModuleFileName(NULL, buffer, MAX_PATH);
-		ShellExecute(NULL, NULL, "cmd.exe", "/c mkdir \"C:\\Program Files\\ContentToClipboard\"", NULL, 0);
-		ShellExecute(NULL, NULL, "cmd.exe", (std::string("/c copy \"") + buffer + "\" \"C:\\Program Files\\ContentToClipboard\\ContentToClipboard.exe\"").c_str(), NULL, 0);
-		if(regAdd(HKEY_CLASSES_ROOT, "*\\shell\\ContentToClipboard", NULL, contextMenuName) != 0
-		|| regAdd(HKEY_CLASSES_ROOT, "*\\shell\\ContentToClipboard\\command", NULL, "\"C:\\Program Files\\ContentToClipboard\\ContentToClipboard.exe\" \"%1\"") != 0){
-			showError("Couldn't create registry keys");
-			errorLevel = 1;
-		}else{
-			MessageBox(NULL, "Installed. Now, you can access the option for copy the contents of a file to the clipboard (may fail in binary files)", "ContentToClipboard::Installation OK", MB_OK);
+		switch(MessageBox(NULL, "Do you want to install this program (YES) or uninstall (NO)? (Requires explicit admin rights)", "ContentToClipboard::Installation", MB_YESNOCANCEL)){
+			case IDYES:
+				GetModuleFileName(NULL, buffer, MAX_PATH);
+				ShellExecute(NULL, NULL, "cmd.exe", "/c mkdir \"C:\\Program Files\\ContentToClipboard\"", NULL, 0);
+				ShellExecute(NULL, NULL, "cmd.exe", (std::string("/c copy \"") + buffer + "\" \"C:\\Program Files\\ContentToClipboard\\ContentToClipboard.exe\"").c_str(), NULL, 0);
+				if(regAdd(HKEY_CLASSES_ROOT, "*\\shell\\ContentToClipboard", NULL, contextMenuName) != 0
+				|| regAdd(HKEY_CLASSES_ROOT, "*\\shell\\ContentToClipboard\\command", NULL, "\"C:\\Program Files\\ContentToClipboard\\ContentToClipboard.exe\" \"%1\"") != 0){
+					showError("Couldn't create registry keys");
+					errorLevel = 1;
+				}else{
+					MessageBox(NULL, "Installed. Now, you can access the option for copy the contents of a file to the clipboard (may fail in binary files)", "ContentToClipboard::Installation OK", MB_OK);
+				}
+				break;
+			case IDNO:
+				ShellExecute(NULL, NULL, "cmd.exe", "/c rd /S /Q \"C:\\Program Files\\ContentToClipboard", NULL, 0);
+				regDelKey(HKEY_CLASSES_ROOT, "*\\shell", "ContentToClipboard");
+					MessageBox(NULL, "Uninstalled", "ContentToClipboard::Uninstalled", MB_OK);
+				break;
+			default:
+				break;
 		}
 	}else if(argc == 2){
 		errorLevel = copyFile(argv[1]);
