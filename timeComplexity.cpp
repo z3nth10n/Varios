becb83242861a23f5cf1a2c2fd866dd76c49ab6b
ivancea <ivancea@mundo-r.com>
2016-04-04 17:13:04 +0200

    Update timeComplexity.cpp

--- timeComplexity.cpp
+++ timeComplexity.cpp
@@ -3,6 +3,9 @@
 #include <vector>
 #include <map>
 #include <ctime>
+#include <thread>
+#include <ratio>
+#include <chrono>
 
 using namespace std;
 
@@ -75,25 +78,6 @@ public:
 	
 };
 
-double bubbleSort(int n){
-	if(n<1)
-		return 0;
-	vector<int> v(n, 0);
-	for(int i=0; i<n; i++)
-		v[i] = rand();
-	
-	clock_t cl = clock();
-	for(int i=0; i<n; i++)
-		for(int j=i+1; j<n; j++)
-			if(v[i]>v[j]){
-				int t = v[i];
-				v[i] = v[j];
-				v[j] = v[i];
-			}
-	
-	return clock()-cl;
-}
-
 vector<Complexity> complexities;
 
 string test(Complexity comp){
@@ -127,25 +111,58 @@ string test(map<int,int>& tests){ // tests : {{input,time},...}
 }
 
 void printFitnesses(Complexity c){
+	cout << ":: FITNESSES ::" << endl;
 	for(const Complexity& comp : complexities){
-		cout << comp.name << ": " << comp.compareTo(c) << endl;
+		cout << ":: " << comp.name << ": " << comp.compareTo(c) << endl;
 	}
+}
+
+double bubbleSort(int n){
+	if(n<1)
+		return 0;
+	vector<int> v(n, 0);
+	for(int i=0; i<n; i++)
+		v[i] = rand();
+	
+	clock_t cl = clock();
+	for(int i=0; i<n; i++)
+		for(int j=i+1; j<n; j++)
+			if(v[i]>v[j]){
+				int t = v[i];
+				v[i] = v[j];
+				v[j] = v[i];
+			}
+	return clock()-cl;
+}
+
+double complexityGenerator(int n){
+	auto t = chrono::high_resolution_clock::now();
+	for(int i=0; i<1000; i++){
+		vector<int> v(n);
+		v.pop_back();
+	}
+	return chrono::duration_cast< chrono::microseconds >(chrono::high_resolution_clock::now()-t).count();
 }
 
 int main(){
 	srand(time(0));
 	int times = 50;
-	complexities.push_back(Complexity([](int n)->double{ return 1; },            times,1,2, "O(1)"));
-	complexities.push_back(Complexity([](int n)->double{ return log(n); },       times,1,2, "O(log n)"));
-	complexities.push_back(Complexity([](int n)->double{ return n; },            times,1,2, "O(n)"));
-	complexities.push_back(Complexity([](int n)->double{ return n*log(n); },     times,1,2, "O(n log n)"));
-	complexities.push_back(Complexity([](int n)->double{ return n*n; },          times,1,2, "O(n^2)"));
-	complexities.push_back(Complexity([](int n)->double{ return n*n * log(n); }, times,1,2, "O(n^2 log n)"));
-	complexities.push_back(Complexity([](int n)->double{ return n*n*n; },        times,1,2, "O(n^3)"));
-	complexities.push_back(Complexity([](int n)->double{ return pow(2, n); },    times,1,2, "O(2^n)"));
-    
-	Complexity bubbleComplexity(&bubbleSort, times,0,5000);
-	Complexity c([](int n)->double{ return 2*log(n); }, times,1,1);
+	
+	cout << "Loading basic complexities (" << times << " cases)..." << endl;
+	
+	complexities.push_back(Complexity([](int n)->double{ return 1; },            times,1,1, "O(1)"));
+	complexities.push_back(Complexity([](int n)->double{ return log(n); },       times,1,1, "O(log n)"));
+	complexities.push_back(Complexity([](int n)->double{ return n; },            times,1,1, "O(n)"));
+	complexities.push_back(Complexity([](int n)->double{ return n*log(n); },     times,1,1, "O(n log n)"));
+	complexities.push_back(Complexity([](int n)->double{ return n*n; },          times,1,1, "O(n^2)"));
+	complexities.push_back(Complexity([](int n)->double{ return n*n * log(n); }, times,1,1, "O(n^2 log n)"));
+	complexities.push_back(Complexity([](int n)->double{ return n*n*n; },        times,1,1, "O(n^3)"));
+	complexities.push_back(Complexity([](int n)->double{ return pow(2, n); },    times,1,1, "O(2^n)"));
+	
+	cout << "Analyzing..." << endl << endl;
+	
+	Complexity c(&complexityGenerator, times,1,10000);
 	printFitnesses(c);
-	cout << "Winner: " << test(bubbleComplexity);
+	
+	cout << "Best fitness: " << test(c);
 }
